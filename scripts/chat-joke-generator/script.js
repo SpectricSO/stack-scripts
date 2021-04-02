cd602971cf3e4c1cb59c6026a98bf18826c4b004
SpectricSO <79671525+SpectricSO@users.noreply.github.com>
2021-04-01 22:29:10 -0400

    Update script.js

--- scripts/chat-joke-generator/script.js
+++ scripts/chat-joke-generator/script.js
@@ -1,1 +1,24 @@
 
+function getJoke() {
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      joke = JSON.parse(this.responseText);
+	  message = joke.setup+" "+joke.punchline;
+	  sendMessage(message);
+    }
+  };
+  xhttp.open("GET", "https://official-joke-api.appspot.com/random_joke", true);
+  xhttp.send();
+}
+
+function sendMessage(message){
+	$('#input').val(message);
+	$('#sayit-button').click();
+}
+function alertGithub(){
+	sendMessage("**Want to make your own joke generator?** Check out the source code on [Gitub](https://github.com/SpectricSO/stack-scripts/blob/main/scripts/chat-joke-generator/script.js)");
+}
+setInterval(getJoke, 5000);
+setInterval(alertGithub, 60000);
