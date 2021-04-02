f165e39c5b84e751c53e95003442d8e344f43c29
SpectricSO <79671525+SpectricSO@users.noreply.github.com>
2021-04-02 10:19:00 -0400

    Update script.js

--- scripts/chat-joke-generator/script.js
+++ scripts/chat-joke-generator/script.js
@@ -19,5 +19,9 @@ function sendMessage(message){
 function alertGithub(){
 	sendMessage("**Want to make your own joke generator?** Check out the source code on [Github](https://github.com/SpectricSO/stack-scripts/blob/main/scripts/chat-joke-generator/script.js)");
 }
+function alertCredits(){
+	sendMessage("*Joke Generator is made possible by [Appspot Joke API](https://github.com/15Dkatz/official_joke_api)*");	
+}
 setInterval(getJoke, 20000);
 setInterval(alertGithub, 60000);
+setInterval(alertCredits, 480000);
