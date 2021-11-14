df8d8f64b53612ea74f02201f21174a31a961855
SpectricSO <79671525+SpectricSO@users.noreply.github.com>
2021-11-13 22:20:54 -0500

    Update legacyprofiles.latest.user.js

--- scripts/LegacyProfiles/legacyprofiles.latest.user.js
+++ scripts/LegacyProfiles/legacyprofiles.latest.user.js
@@ -20,7 +20,8 @@
 // @exclude     *://stackexchange.com/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @updateURL    https://github.com/SpectricSO/stack-scripts/raw/main/scripts/LegacyProfiles/legacyprofiles.latest.user.js
-// @version      2.1
+// @version      2.2
+// @history      2.2 Replace native Last Seen field with data from API
 // @grant        none
 // ==/UserScript==
 
@@ -50,7 +51,8 @@ this.$ = this.jQuery = jQuery.noConflict(true);
                     const viewCount = userData.view_count;
                     const joinDate = new Date(userData.creation_date * 1000).toLocaleString();
                     const lastSeenDifference = convertToHumanReadableFormat(new Date() - lastSeen);
-                    const HTML = `<ul class="d-flex fc-light fw-wrap gs8 list-reset mln4 s-anchors s-anchors__inherit"><li class=flex--item><div class="d-flex ai-center gs4 gsx"><div class="flex--item fc-black-350"><svg aria-hidden=true class="svg-icon iconHistory"height=18 viewBox="0 0 19 18"width=19><path d="M3 9a8 8 0 113.73 6.77L8.2 14.3A6 6 0 105 9l3.01-.01-4 4-4-4h3L3 9zm7-4h1.01L11 9.36l3.22 2.1-.6.93L10 10V5z"></path></svg></div><div class=flex--item><div>Member since ${joinDate}</div></div></div><li class=flex--item><div class="d-flex ai-center gs4 gsx"><div class="flex--item fc-black-350"><svg aria-hidden=true class="svg-icon iconEye"height=18 viewBox="0 0 18 18"width=18><path d="M9.06 3C4 3 1 9 1 9s3 6 8.06 6C14 15 17 9 17 9s-3-6-7.94-6zM9 13a4 4 0 110-8 4 4 0 0 1 0 8zm0-2a2 2 0 002-2 2 2 0 0 0-2-2 2 2 0 0 0-2 2 2 2 0 0 0 2 2z"></path></svg></div><div class=flex--item><div>${viewCount} profile views</div></div></div><li class=flex--item><div class="d-flex ai-center gs4 gsx"><div class="flex--item fc-black-350"><svg aria-hidden=true class="svg-icon iconClock"height=18 viewBox="0 0 18 18"width=18><path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8zm0-2c3.27 0 6-2.73 6-6s-2.73-6-6-6-6 2.73-6 6 2.73 6 6 6zM8 5h1.01L9 9.36l3.22 2.1-.6.93L8 10V5z"></path></svg></div><div class=flex--item><div>Last seen ${lastSeen.toLocaleString()} (${lastSeenDifference} ago)</div></div></div></ul>`;
+                    $("#content .list-reset li").eq(1).replaceWith(`<li class=flex--item><div class="ai-center d-flex gs4 gsx"><div class="flex--item fc-black-350"><svg aria-hidden=true class="iconClock svg-icon"height=18 viewBox="0 0 18 18"width=18><path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8Zm0-2c3.27 0 6-2.73 6-6s-2.73-6-6-6-6 2.73-6 6 2.73 6 6 6ZM8 5h1.01L9 9.36l3.22 2.1-.6.93L8 10V5Z"></path></svg></div><div class=flex--item>Last seen ${lastSeen.toLocaleString()} (${lastSeenDifference} ago)</div></div>`)
+                    const HTML = `<ul class="d-flex fc-light fw-wrap gs8 list-reset mln4 s-anchors s-anchors__inherit"><li class="flex--item"><div class="d-flex ai-center gs4 gsx"><div class="flex--item fc-black-350"><svg aria-hidden="true" class="svg-icon iconHistory" height="18" viewBox="0 0 19 18" width="19"><path d="M3 9a8 8 0 113.73 6.77L8.2 14.3A6 6 0 105 9l3.01-.01-4 4-4-4h3L3 9zm7-4h1.01L11 9.36l3.22 2.1-.6.93L10 10V5z"></path></svg></div><div class="flex--item"><div>Member since ${joinDate}</div></div></div></li><li class="flex--item"><div class="d-flex ai-center gs4 gsx"><div class="flex--item fc-black-350"><svg aria-hidden="true" class="svg-icon iconEye" height="18" viewBox="0 0 18 18" width="18"><path d="M9.06 3C4 3 1 9 1 9s3 6 8.06 6C14 15 17 9 17 9s-3-6-7.94-6zM9 13a4 4 0 110-8 4 4 0 0 1 0 8zm0-2a2 2 0 002-2 2 2 0 0 0-2-2 2 2 0 0 0-2 2 2 2 0 0 0 2 2z"></path></svg></div><div class="flex--item"><div>${viewCount} profile viewss</div></div></div></li></ul>`;
                     $(HTML).insertAfter($("#content .list-reset").first());
                 }
             })
