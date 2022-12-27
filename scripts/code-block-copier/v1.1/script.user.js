0cf86cc7dc55d6a2ea6a0c6838080885b12f9438
SpectricSO <79671525+SpectricSO@users.noreply.github.com>
2022-12-26 21:28:38 -0500

    Update script.user.js

--- scripts/code-block-copier/v1.1/script.user.js
+++ scripts/code-block-copier/v1.1/script.user.js
@@ -164,7 +164,7 @@
     });
 })();
 /*
-Copyright 2021 <Spectric @ https://stackoverflow.com/users/14251221/spectric>
+Copyright 2022 <Spectric @ https://stackoverflow.com/users/14251221/spectric>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
